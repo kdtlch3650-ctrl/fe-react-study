417fce950dfa505fe8cd961bb3306d5de811632a
kdtlch3650-ctrl <kdtlch3650@gmail.com>
2025-12-05 09:28:34 +0900

    뉴스 블로그

--- src/newsBlog/NewsBlog.js
+++ src/newsBlog/NewsBlog.js
@@ -13,9 +13,16 @@ function NewsBlog() {
 
     let [news, setNews] = useState(['오늘의 뉴스', '어제의 뉴스', '내일의 뉴스']);
 
-    let [likeCount, setLikeCount] = useState(0);
+    let [likeCount, setLikeCount] = useState([0, 0, 0]);
 
     let [modalFlag, setModalFlag] = useState(false);
+
+    let ptitle = ['1제목', '2제목', '3제목'];
+    let pday = ['1', '2', '3'];
+    let pcontent = ['1내용', '2내용', '3내용'];
+
+    let count = [0, 1, 2];
+    let [pcount,setpcount] = useState(0);
     return (
         <div>
             <div className="black-nav">
@@ -23,9 +30,48 @@ function NewsBlog() {
                 <div style={{ color: "orange", fontSize: "20px" }}>{title}</div>
             </div>
 
+            {
+                news.map((item, index) => {
+                    return (                                           
+                        <div div className = "post-list" >
+                            <h4 onClick={() => {
+
+                                console.log(index);
+                                setpcount(pcount = index);
+                                setModalFlag(!modalFlag); //!true => false;
+
+                            }}>{item} <span onClick={(event) => {
+
+                                event.stopPropagation(); //이벤트 발생을 추가로 전파(전달) stop
+                                let copy = [...likeCount];
+                                copy[index]++;
+                                setLikeCount(copy); //하트 갯수 증가 + 재랜더링
+                                
+                            }}>🖤</span>{likeCount[index]}
+                            </h4>
+                            <p>내용</p>
+                        </div>
+    )
+})
+            }
 
 
-            <div className="post-list">
+{/* <div className="post-list">
+                <h4 onClick={() => {
+                    setModalFlag(!modalFlag); //!true => false;
+                }}>{news[0]} <span onClick={(event) => {
+                    event.stopPropagation(); //이벤트 발생을 추가로 전파(전달) stop
+                    setLikeCount(likeCount++) //하트 갯수 증가 + 재랜더링
+                }}>🖤</span>{likeCount}
+                </h4>
+                <p>내용</p>
+            </div> */}
+
+
+
+
+
+{/* <div className="post-list">
                 <h4 onClick={() => {
                     // 순수 함수 영역이기에 if 문 가능
                     //return 밖에다 하는것과 같음
@@ -47,16 +93,8 @@ function NewsBlog() {
                 }}>🖤</span>{likeCount}
                 </h4>
                 <p>내용</p>
-            </div>
-            <div className="post-list">
-                <h4>{news[1]}</h4>
-                <p>내용</p>
-            </div>
-            <div className="post-list">
-                <h4>{news[2]}</h4>
-                <p>내용</p>
-            </div>
-            {/* <div className="post-list">
+            </div> */}
+{/* <div className="post-list">
                 <h4>{news[0]}</h4>
                 <p>내용</p>
             </div>
@@ -69,13 +107,15 @@ function NewsBlog() {
                 <p>내용</p>
             </div> */}
 
-            {
-                // modalFlag == true ? <Modal/>:null
-            }
-            {
-                modalFlag && <Modal />
-            }
-        </div>
+{
+    // modalFlag == true ? <Modal/>:null
+}
+{
+    // modalFlag && console.log(ptitle[pcount])
+    modalFlag && <Modal probs={{ ptitle: ptitle[pcount], pday: pday[pcount], pcontent: pcontent[pcount] }} />
+    // modalFlag && <Modal probs={{ ptitle : title, pday : title, pcontent : title}} />
+}
+        </div >
     );
 }
 export default NewsBlog;
